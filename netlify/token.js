c73f1472371ac6ff5402a25fbeb0e28775d430c4
abdulakeem eletu <abdulakeem.eletu@telegraph.co.uk>
2024-05-31 17:02:06 +0100

    Initial commit

---
+++ netlify/token.js
@@ -0,0 +1,23 @@
+// netlify/functions/token.js
+const twilio = require('twilio');
+const AccessToken = twilio.jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+
+exports.handler = async function(event, context) {
+  const { identity } = JSON.parse(event.body);
+
+  const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+  const twilioApiKey = process.env.TWILIO_API_KEY;
+  const twilioApiSecret = process.env.TWILIO_API_SECRET;
+
+  const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
+  token.identity = identity;
+
+  const videoGrant = new VideoGrant();
+  token.addGrant(videoGrant);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ token: token.toJwt() })
+  };
+};
