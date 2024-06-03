671fc6bdc503712f8a5341902086b1b308dc83cc
abdulakeem eletu <abdulakeem.eletu@telegraph.co.uk>
2024-06-03 14:25:17 +0100

    adding styles-3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
+import Signup from './Signup';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 
@@ -22,7 +23,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          {/* Add other routes here */}
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Router>
     </ThemeProvider>
